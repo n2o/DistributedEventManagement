f4e4be4055427d9b1c9e2e658ac7008b1077c242
n2o <cmeter@googlemail.com>
2013-08-29 21:19:06 +0200

    [thesis] Added new graphics to explain polling and websockets

--- anwendung/cakePHP/app/Controller/Component/OtherComponent.php
+++ anwendung/cakePHP/app/Controller/Component/OtherComponent.php
@@ -1,15 +1,6 @@
 <?php 
 App::uses('Component', 'Controller');
 class OtherComponent extends Component {
-
-	/**
-	 * Create notification on screen
-	 * Possible types: alert, success, error, warning, information, confirm
-	 */
-	public function noty($text, $type) {
-		echo "<script>noty({text: $text, type: $type});</script>";
-	}
-
 	/**
 	 * Send WebSockets via PHP. Used to publish changes in database
 	 *

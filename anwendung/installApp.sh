7d3a6a641e91d41be580eb9ce7ed448485e0cbad
n2o <cmeter@googlemail.com>
2013-09-12 21:42:56 +0200

    Developing at shell script...

--- anwendung/installApp.sh
+++ anwendung/installApp.sh
@@ -12,15 +12,36 @@ echo "#                                                      #"
 echo "########################################################"
 echo " "
 echo "# Checking for latest version of your distribution..."
+echo " "
 
 aptitude update && aptitude upgrade
 
+echo " "
 echo "# Done."
-
+echo " "
+echo "# Now downloading and installing Apache2, PHP5, MySQL, phpmyadmin, openssl and current version of JavaScript..."
 echo " "
 
-echo "# Now downloading and installing Apache2, PHP5, MySQL, phpmyadmin and current version of JavaScript..."
+apt-get install apache2 php5 libapache2-mod-php5 mysql-server mysql-client php-pear php5-suhosin php5-mysql phpmyadmin javascript-common openssl
 
-apt-get install apache2 php5 libapache2-mod-php5 mysql-server mysql-client php5-mysql phpmyadmin javascript-common
-
+echo " "
 echo "# Done."
+echo " "
+echo "# Enabling openSSL to create private / public key pair"
+echo " "
+
+sed -i 's/;(extension=openssl.so)/\1/' /etc/php.ini
+sed -i 's/;(extension=openssl.so)/\1/' /etc/php5/apache2/php.ini
+
+echo " "
+echo "# Done."
+echo " "
+echo "# Now moving the Meissner webpage to /var/www and changing user privilegs for www-data..."
+
+mv meissner/ /var/www/meissner
+chown -R www-data:www-data /var/www/meissner
+chmod -R 755 /var/www/meissner
+
+echo " "
+echo "# Done."
+echo " "

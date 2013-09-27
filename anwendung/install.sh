ca6a71c9f4b9264eb57523921dd70f963bd6af23
Philipp Hagemeister <phihag@phihag.de>
2013-09-27 06:36:22 +0200

    Simplify debian install script
    set -e exits the script if something fails.
    Prefixing all root operations with sudo makes it work by default on Ubuntu and debian setups with sudo (recommended).
    phpmyadmin is not necessary.
    node shouldn't be compiled as root, and xdg-open should not be run as root.
    Use -y flag to apt-get to cut down messages.
    Create tmp directory automatically (it's not checked in to git).
    Less fluff in the output.

--- anwendung/install.sh
+++ anwendung/install.sh
@@ -4,88 +4,57 @@
 # Christian Meter, September 2013
 
 
-clear
-echo "###########################################"
-echo "#                                         #"
-echo "# Installation of Meissner Webapplication #"
-echo "#                                         #"
-echo "###########################################"
-echo ""
+set -e
+
 echo "# Checking for latest version of your distribution..."
-echo ""
 
-aptitude update && aptitude upgrade
+sudo aptitude update && sudo aptitude dist-upgrade -y
 
-echo ""
-echo "# Done."
-echo ""
-echo "# Now downloading and installing Apache2, PHP5, MySQL, phpmyadmin, openssl and current version of JavaScript..."
-echo ""
+echo "# Now downloading and installing Apache2, PHP5, MySQL, openssl and current version of JavaScript..."
 
-apt-get install apache2 mysql-server mysql-client php5 libapache2-mod-php5 php-pear php5-mysql php5-curl php5-common php5-cli php5-gd phpmyadmin javascript-common make python gcc g++
+sudo apt-get install -y apache2 mysql-server mysql-client php5 libapache2-mod-php5 php-pear php5-mysql php5-curl php5-common php5-cli php5-gd javascript-common make python gcc g++
 
-echo ""
-echo "# Done."
-echo ""
 echo "# Enabling cURL and mod_rewrite..."
 
 # Enabling curl in php.ini
-grep -q -e 'extension=curl.so' /etc/php5/apache2/php.ini || sed -i.bak '$ a\\n; Adding module curl for Meissner Webapplication\nextension=curl.so' /etc/php5/apache2/php.ini
+grep -q -e 'extension=curl.so' /etc/php5/apache2/php.ini || sudo sed -i.bak '$ a\\n; Adding module curl for Meissner Webapplication\nextension=curl.so' /etc/php5/apache2/php.ini
 
 # Enabling mod_rewrite in apache2.conf
-grep -q -e 'LoadModule rewrite_module modules/apache2/mod_rewrite.so' /etc/apache2/apache2.conf || sed -i.bak '$ a\\n# Enabling mod_rewrite\nLoadModule rewrite_module modules/apache2/mod_rewrite.so' /etc/apache2/apache2.conf
+grep -q -e 'LoadModule rewrite_module modules/apache2/mod_rewrite.so' /etc/apache2/apache2.conf || sudo sed -i.bak '$ a\\n# Enabling mod_rewrite\nLoadModule rewrite_module modules/apache2/mod_rewrite.so' /etc/apache2/apache2.conf
 
-# Enabling FollowSymLinks for meissner app
-grep -q -e '<Directory "/var/www/meissner2">' /etc/apache2/apache2.conf || sed -i.bak '$ a\\n# Allowing SymLinks for Meissner Webapplication\n<Directory "/var/www/meissner">\n\tOptions FollowSymLinks\n\tAllowOverride All\n</Directory>' /etc/apache2/apache2.conf
+# Enabling FollowSymLinks for fmeissner app
+grep -q -e '<Directory "/var/www/meissner2">' /etc/apache2/apache2.conf || sudo sed -i.bak '$ a\\n# Allowing SymLinks for Meissner Webapplication\n<Directory "/var/www/meissner">\n\tOptions FollowSymLinks\n\tAllowOverride All\n</Directory>' /etc/apache2/apache2.conf
 
-a2enmod rewrite
+sudo a2enmod rewrite
 
-echo ""
-echo "# Done."
-echo ""
 echo "# Now moving the Meissner webpage to /var/www and changing user privilegs for www-data..."
 
-cp -R meissner/ /var/www/
-chown -R www-data:www-data /var/www/meissner
-chmod -R 755 /var/www/meissner
-chmod -R 777 /var/www/meissner/app/tmp
+sudo cp -R meissner/ /var/www/
+sudo chown -R www-data:www-data /var/www/meissner
+sudo chmod -R 755 /var/www/meissner
+sudo mkdir -p /var/www/meissner/app/tmp
+sudo chmod -R 777 /var/www/meissner/app/tmp
 
-echo ""
-echo "# Done."
-echo ""
 echo "# Restarting apache..."
-echo ""
 
-/etc/init.d/apache2 stop
-/etc/init.d/apache2 start
+sudo /etc/init.d/apache2 stop
+sudo /etc/init.d/apache2 start
 
-echo ""
-echo "# Done."
-echo ""
 echo "# Finally installing node.js as WebSocket Server..."
-echo ""
 
 cd /tmp/
-wget http://nodejs.org/dist/node-latest.tar.gz
+wget -c http://nodejs.org/dist/node-latest.tar.gz
 tar -xzvf node-latest.tar.gz
 rm node-latest.tar.gz*
 cd node-*
 ./configure
 make
-make install
+sudo make install
 
-echo ""
-echo "# Done."
-echo ""
 echo "# At last starting WebSocket Server in /var/www/meissner/app/webroot/websocket/socket-server.js..."
-echo ""
 
 node /var/www/meissner/app/webroot/websocket/socket-server.js &
 
-echo ""
-echo "# Completed!"
-echo ""
 echo "# To setup the MySQL connection, just type localhost/meissner/setup in your webbrowser."
-echo ""
 
 xdg-open http://localhost/meissner/setup

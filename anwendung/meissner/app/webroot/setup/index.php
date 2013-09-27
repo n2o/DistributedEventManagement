3c03cd1e56781a312882b79da2988088e847a175
n2o <cmeter@googlemail.com>
2013-09-27 09:47:27 +0200

    Optimizing .gitgnores

---
+++ anwendung/meissner/app/webroot/setup/index.php
@@ -0,0 +1,43 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	<link rel="stylesheet" type="text/css" href="../css/main.css" />
+	<link rel="stylesheet" type="text/css" href="../css/default.css" />
+
+	<title>Installing the Mei&szlig;ner Application</title>
+</head>
+<body>
+	<div id="container">
+		<section id="page">
+			<header>
+				<img src="../img/logo/logo.png" width="100px">
+			</header>
+
+			<div id="content">
+				<article>
+					<h1>Installing the Mei&szlig;ner Application</h1>
+					<h4>Welcome to the installation of the fabulous Mei&szlig;ner Application.</h4>
+					<br>
+					<p>
+						That you can see this page, shows, that your LAMP server is set up properly. So in the next steps we will create the correct
+						structure for the MySQL Database. 
+					</p>
+					<br>
+					<p>
+						For this you need an valid account to your database, the password for this user and the location of the database.
+					</p>
+					<br><br>
+					<p><a href="step1.php" class="button">Next</a></p>
+
+				</article>
+			</div>
+			<footer>
+			</footer>
+		</section>
+	</div>
+</body>
+</html>
